69f927e7da4cf1d1dddff727132535282abdc2b4
Ju4N 4lvaR3Z <Ju4n-4lvaR3Z@users.noreply.github.com>
2023-05-26 15:51:32 -0400

    Archivos Semestre 1

--- retos/reto5.py
+++ retos/reto5.py
@@ -7,14 +7,20 @@ if num%2==0:
 else:
     par="inpar"
 n=2
+n2=3
 if n>=num:
     pri="Es primo"
 elif num%n!=0:
     pri="Es primo"
+    if num!=n2:
+        if num%n2!=0:   
+            pri="Es primo"  
+        else:
+          pri="No es primo"
 else:
     pri="No es primo"
 if num<50:
     c="Es menor a 50"
 else:
     c="Es mayor a 50"
-print(f"El numero {num} es {par}, {pri} y {c}")
+print(f"El numero {num} es,{pri} y {c}")
