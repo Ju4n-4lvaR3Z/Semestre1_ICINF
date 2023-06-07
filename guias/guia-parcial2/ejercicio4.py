3e0d9ac2512ad097ca7efb80bc51d2d8bbd73b97
Ju4N 4lvaR3Z <Ju4n-4lvaR3Z@users.noreply.github.com>
2023-06-07 14:25:59 -0400

    guia-parcial-2

---
+++ guias/guia-parcial2/ejercicio4.py
@@ -0,0 +1,22 @@
+def vuelto(p,vp):
+    while True:
+        billetera=input("Ingrese con cuanto paga\n>")
+        try :
+            billetera=int(billetera)
+            vp=int(vp)
+            r=vp%10
+            if r>=5:
+                vp=vp+(10-r)
+            elif r<=4:
+                vp=vp-(r)
+            if billetera>=vp:
+                r=billetera-vp
+                break
+            else:
+                e=int("s")
+        except:
+            print("Ingrese un valor aceptable")
+    return r
+producto=input("Que producto quiere comprar?\n>")
+Vproducto=input(f"Ingrese el valor de {producto}\n>")
+print("Su vuelto es $",vuelto(producto,Vproducto))
